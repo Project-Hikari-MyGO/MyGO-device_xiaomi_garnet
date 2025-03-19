71caf547d7ed10dce19dffe592488dc155ff05d7
UnmoveD <unmoved2121@gmail.com>
2025-03-19 20:45:41 -0300

    garnet: Patch nfc_nci.nqx.default.hw.v1 blob to load libbase_shim
    error: Unresolved symbol: _ZN7android4base4TrimERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE out/soong/.intermediates/vendor/xiaomi/garnet/nfc_nci.nqx.default.hw.v1/android_vendor_arm64_armv8-a-branchprot_shared/nfc_nci.nqx.default.hw.v1.so: note: out/soong/.intermediates/vendor/xiaomi/garnet/nfc_nci.nqx.default.hw.v1/android_vendor_arm64_armv8-a-branchprot_shared/nfc_nci.nqx.default.hw.v1.so: note: Some dependencies might be changed, thus the symbol(s) above cannot be resolved. out/soong/.intermediates/vendor/xiaomi/garnet/nfc_nci.nqx.default.hw.v1/android_vendor_arm64_armv8-a-branchprot_shared/nfc_nci.nqx.default.hw.v1.so
    
    Change-Id: I32aef292b843daef38f5809ddd66b4cbd28afe1c

--- extract-files.py
+++ extract-files.py
@@ -105,7 +105,9 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/lib64/libqcrilNr.so', 'vendor/lib64/libril-db.so'): blob_fixup()
         .binary_regex_replace(rb'persist\.vendor\.radio\.poweron_opt', rb'persist.vendor.radio.poweron_ign'), 
     'vendor/lib64/libkaraokepal.so': blob_fixup()
-        .replace_needed('audio.primary.parrot.so', 'audio.primary.garnet.so'),         
+        .replace_needed('audio.primary.parrot.so', 'audio.primary.garnet.so'),
+    'vendor/lib64/nfc_nci.nqx.default.hw.v1.so': blob_fixup()
+        .add_needed('libbase_shim.so'),             
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
         .add_needed('libhidlbase_shim.so'),
 }  # fmt: skip
